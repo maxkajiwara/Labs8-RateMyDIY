9463d6116573b4c73bb255247b7940af1ba9ef20
michaelagard <mdagard@gmail.com>
2018-11-07 13:42:07 -0500

    export UserSettingsSideBar

--- reactapp/src/components/index.js
+++ reactapp/src/components/index.js
@@ -1,23 +1,10 @@
 export { default as App } from "./__App/App";
-export {
-  default as ExampleComponent
-} from "./_ExampleComponent/ExampleComponent";
-export {
-  default as FeaturedProjects
-} from "./_LandingPage/FeaturedProjects/FeaturedProjects.js";
-
-export {
-  default as PopularMakers
-} from "./_LandingPage/PopularMakers/PopularMakers.js";
-
-export {
-  default as PopularReviewers
-} from "./_LandingPage/PopularReviewers/PopularReviewers.js";
-
-export { default as LandingPage } from "./_LandingPage/LandingPage.js";
-
-export { default as ProjectTile } from "./ProjectTile/ProjectTile.js";
-
-export { default as MakerTile } from "./MakerTile/MakerTile.js";
-
-export { default as ReviewerTile } from "./ReviewerTile/ReviewerTile.js";
+export { default as ExampleComponent } from "./_ExampleComponent/ExampleComponent";
+export { default as FeaturedProjects } from "./_LandingPage/FeaturedProjects/FeaturedProjects";
+export { default as PopularMakers } from "./_LandingPage/PopularMakers/PopularMakers";
+export { default as PopularReviewers } from "./_LandingPage/PopularReviewers/PopularReviewers";
+export { default as LandingPage } from "./_LandingPage/LandingPage";
+export { default as ProjectTile } from "./ProjectTile/ProjectTile";
+export { default as MakerTile } from "./MakerTile/MakerTile";
+export { default as ReviewerTile } from "./ReviewerTile/ReviewerTile";
+export { default as UserSettingsSideBar } from "./_UserSettingsPage/SideBar/SideBar"
