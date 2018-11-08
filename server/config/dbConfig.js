1f92656849574cced4d3f4eb4a25838d341edfc1
alejandrok93 <alejandrok93@gmail.com>
2018-11-08 10:34:26 -0800

    Added temporary config for heroku POSGRESQL database, routes for projects

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,4 +1,9 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile');
+const knex = require("knex");
+const knexConfig = require("../knexfile");
 
 module.exports = knex(knexConfig.auth);
+
+//added HEROKU POSGRESQL DB for testing
+//module.exports = knex(knexConfig.heroku);
+// -> not sure how to set up external HEROKU DB
+//DATABASE_URL=heroku pg:psql postgresql-rigid-75651 --app labs8ratemydiy
--- server/knexfile.js
+++ server/knexfile.js
@@ -2,50 +2,66 @@
 const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
-	auth: {
-		client: 'sqlite3',
-    	connection: {
-      		filename: './auth/db.sqlite3'
-    	},
-    	useNullAsDefault: true,
-    	migrations: {
-      		directory: './auth/migrations'
-    	},
-    	seeds: {
-      		directory: './auth/seeds'
-    	}
-	},
-	development: {
-		client: 'mssql',
-		connection: {
-			host: 'localhost\\SQLEXPRESS',
-			user: 'sa',
-			password: 'admin',
-			database: 'diydb',
-			options: {
-				port: 1433,
-				encrypt: true
-			}
-		},
-		useNullAsDefault: true,
-		migrations: {
-			directory: './migrations',
-			tableName: 'migrations'
-		},
-		seeds: { directory: './seeds' }
-	},
+  auth: {
+    client: "sqlite3",
+    connection: {
+      filename: "./auth/db.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./auth/migrations"
+    },
+    seeds: {
+      directory: "./auth/seeds"
+    }
+  },
+  development: {
+    client: "mssql",
+    connection: {
+      host: "localhost\\SQLEXPRESS",
+      user: "sa",
+      password: "admin",
+      database: "diydb",
+      options: {
+        port: 1433,
+        encrypt: true
+      }
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./migrations",
+      tableName: "migrations"
+    },
+    seeds: { directory: "./seeds" }
+  },
 
-	production: {
-		client: 'mssql',
-		connection: dbConnection,
-		pool: {
-			min: 2,
-			max: 10
-		},
-		migrations: {
-			directory: './migrations',
-			tableName: 'migrations'
-		},
-		seeds: { directory: './seeds' }
-	}
+  production: {
+    client: "mssql",
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./migrations",
+      tableName: "migrations"
+    },
+    seeds: { directory: "./seeds" }
+  },
+
+  heroku: {
+    client: "pg",
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./migrations"
+    },
+    seeds: {
+      directory: "./seeds"
+    }
+  }
 };
