cc361a6f56e73984c927cf8f4e48b9c781a7f90f
Tristen Neu <tristenneu@Tristens-iMac.local>
2018-11-09 11:37:22 -0600

    Sign up / sign in component

---
+++ reactapp/src/components/Auth/Auth.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const Auth = () => {
+    return (
+            <div>
+              <a href="http://localhost:5000/signin">Sign Up or Sign In</a>
+            </div>
+          );
+};
+
+export default Auth;
