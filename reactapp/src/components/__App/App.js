cd4dabb702e61223d3bf94fd972b1a39d2389bce
alejandrok93 <alejandrok93@gmail.com>
2018-11-13 09:37:33 -0800

    moved Searcbar component to fix bug

--- reactapp/src/components/__App/App.js
+++ reactapp/src/components/__App/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
 import styled from "styled-components";
-import Auth from '../../components/Auth/Auth';
+import Auth from "../../components/Auth/Auth";
 
 // Components
 import {
@@ -10,7 +10,7 @@ import {
   ReviewList,
   LandingPage,
   ProjectList,
-  CreateEditPage, 
+  CreateEditPage,
   Billing,
   UserSettingsSideBar,
   UserSettingsSummaries,
@@ -20,9 +20,8 @@ import {
   UserSettingSkills,
   UserSettingResumes,
   UserSettingSettings,
- SearchBar,
-  SearchPage,
-
+  SearchBar,
+  SearchPage
 } from "../../components";
 
 //Styles
@@ -58,7 +57,7 @@ class App extends Component {
           </li>
         </ul>
         {/* <Navbar /> */}
-        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/" component={SearchPage} />
         <Route path="/settings" component={UserSettingsSideBar} />
         <Route path="/settings/summaries" component={UserSettingsSummaries} />
         <Route exact path="/ReviewList" component={ReviewList} />
