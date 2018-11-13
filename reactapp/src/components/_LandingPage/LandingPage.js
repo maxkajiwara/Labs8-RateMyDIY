34d0746eb731a865568d23dd67dc274dde7037c5
alejandrok93 <alejandrok93@gmail.com>
2018-11-13 09:34:23 -0800

    commented out search bar

--- reactapp/src/components/_LandingPage/LandingPage.js
+++ reactapp/src/components/_LandingPage/LandingPage.js
@@ -40,7 +40,7 @@ class LandingPage extends Component {
     // console.log(SearchBar);
     return (
       <div className="container">
-        <SearchBar handleChange={this.handleChange} />
+        {/* <SearchBar handleChange={this.handleChange} /> */}
         <div className="landing-page-container">
           <FeaturedProjects featuredProjects={this.props.featuredProjects} />
           <PopularMakers popularMakers={this.props.popularMakers} />
