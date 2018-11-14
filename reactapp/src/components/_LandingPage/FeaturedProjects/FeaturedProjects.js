55dd77ab26414eaa9a82ad58a74be3e98d733b0c
michaelagard <mdagard@gmail.com>
2018-11-14 13:27:04 -0500

    removed css, added styled components

--- reactapp/src/components/_LandingPage/FeaturedProjects/FeaturedProjects.js
+++ reactapp/src/components/_LandingPage/FeaturedProjects/FeaturedProjects.js
@@ -1,12 +1,30 @@
 // Import Dependencies
 import React, { Component } from "react";
 // import { Link } from "react-router-dom";
-
+import styled from 'styled-components';
 //Import components
 import { ProjectTile } from "../../../components";
 
 //Import Styling
-import "./FeaturedProjects.css";
+const FeaturedProjectsWrapper = styled.div`
+  width: 100%;
+  margin: 20px auto;
+  border: 1px solid blue;
+`;
+
+const FeaturedProjectTitle = styled.h1`
+  font-size: 18px;
+  width: 100%;
+  margin: 10px 25px;  
+`;
+
+const ProjectWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
 class FeaturedProjects extends Component {
   // constructor() {
   //   super();
@@ -14,14 +32,14 @@ class FeaturedProjects extends Component {
 
   render() {
     return (
-      <div className="projects-container">
-        <h1>Featured Projects</h1>
-        <div className="projects">
+      <FeaturedProjectsWrapper>
+        <FeaturedProjectTitle>Featured Projects</FeaturedProjectTitle>
+        <ProjectWrapper>
           {this.props.featuredProjects.map(project => (
             <ProjectTile project={project} />
           ))}
-        </div>
-      </div>
+        </ProjectWrapper>
+      </FeaturedProjectsWrapper>
     );
   }
 }
