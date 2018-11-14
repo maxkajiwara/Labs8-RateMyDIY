f5c03d86ee1ec3f15d71de4ae6e7ba82be43a5e1
Max <maxkajiwara@gmail.com>
2018-11-14 14:57:56 -0800

    Update project timestamp when post is added or updated

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -15,11 +15,19 @@ function addPost(user_id, project_id, post) {
 				return db('posts')
 					.returning('post_id')
 					.insert({ ...post, project_id })
-					.then(([id]) => id);
-			} else return undefined;
+					.then(([id]) => {
+						if (id) {
+							return db('projects')
+								.where({ project_id })
+								.update({ last_updated: db.fn.now() })
+								.then(() => id);
+						}
+					});
+			} else return undefined; // no project by that id || wrong author
 		});
 }
 
+// I feel like this could be more elegant.
 function editPost(user_id, project_id, post_id, changes) {
 	return db('projects')
 		.where({ user_id, project_id })
@@ -27,11 +35,20 @@ function editPost(user_id, project_id, post_id, changes) {
 		.then(post => {
 			if (post) {
 				return db('posts')
-					.where({ post_id, project_id })
+					.where({ project_id, post_id })
 					.returning('post_id')
 					.update(changes)
-					.then(ids => ids.length);
-			} else return undefined;
+					.then(ids => ids.length)
+					.then(count => {
+						if (count) {
+							return db('projects')
+								.where({ project_id })
+								.returning('project_id')
+								.update({ last_updated: db.fn.now() })
+								.then(ids => ids.length);
+						} else return undefined; // no post by that id
+					});
+			} else return undefined; // no project by that id || wrong author
 		});
 }
 
@@ -39,11 +56,11 @@ function removePost(user_id, project_id, post_id) {
 	return db('projects')
 		.where({ user_id, project_id })
 		.first()
-		.then(post => {
-			if (post) {
+		.then(project => {
+			if (project) {
 				return db('posts')
-					.where({ post_id })
+					.where({ project_id, post_id })
 					.del();
-			} else return undefined;
+			} else return undefined; // no post by that id || wrong author
 		});
 }
