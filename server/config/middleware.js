faffd22398da6bb2cb848fa54bf8efbdc2f98653
alejandrok93 <alejandrok93@gmail.com>
2018-11-14 09:51:15 -0800

    Created new branch to add Authenticate middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const passport = require('passport');
-const strategy = require('../setup-passport');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-var KnexSessionStore = require('connect-session-knex')(session);
-const db = require('./dbConfig');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const passport = require("passport");
+const strategy = require("../setup-passport");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+var KnexSessionStore = require("connect-session-knex")(session);
+const db = require("./dbConfig");
 var store = new KnexSessionStore({
-	knex: db,
-	tablename: 'sessions',
-	sidfieldname: 'sid',
-	createtable: false
+  knex: db,
+  tablename: "sessions",
+  sidfieldname: "sid",
+  createtable: false
 });
 
 const sessionConfig = {
-	store: store,
-	secret: [process.env.SESSION_SECRET || 'TKd8^S$W-HPS3NtF'],
-	cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }, // 1 week
-	resave: false,
-	saveUninitialized: false
+  store: store,
+  secret: [process.env.SESSION_SECRET || "TKd8^S$W-HPS3NtF"],
+  cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }, // 1 week
+  resave: false,
+  saveUninitialized: false
 };
+
+function authenticate(req, res, next) {}
 
 module.exports = server => {
   //removed Logger middleware to debug server endpoints
