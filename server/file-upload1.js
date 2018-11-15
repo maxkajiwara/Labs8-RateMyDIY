f935c9cff6fc510f7531866e3b30947f1c3b63a8
michaelagard <mdagard@gmail.com>
2018-11-15 14:07:22 -0500

    commented code on what it does

--- server/file-upload1.js
+++ server/file-upload1.js
@@ -12,7 +12,7 @@ aws.config.update({
   accessKeyId: process.env.AWSAccessKeyId,
   region: 'us-east-1' // region of your bucket
 });
-
+// sets new instance of aws s3
 const s3 = new aws.S3();
 
 const multer = require('multer');
@@ -20,9 +20,15 @@ const multerS3 = require('multer-s3');
 
 const upload = multer({
   storage: multerS3({
+    // allows for new instances of s3 created from above
     s3: s3,
+    // name of bucket on aws s3
     bucket: 'ratemydiy',
+    // access control for the file (‘public read’ means that anyone can view files)
+    // you can check all the available types here:
+    // https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl 
     acl: 'public-read',
+    // metadata stored on asw s3
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
     },
